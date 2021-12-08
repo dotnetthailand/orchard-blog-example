7b7500cea828deca21d52a20685386a5feb0f329
aaronamm <theeranitp@gmail.com>
2021-12-08 17:12:49 +0700

    Add simple Apollo GraphQL client

--- src/Modules/OrchardExample.ReactClient/src/main.tsx
+++ src/Modules/OrchardExample.ReactClient/src/main.tsx
@@ -1,8 +1,64 @@
+// Enable GraphQL module
+// Edit Role Anonymous, Execute GraphQL to allow
 import 'react';
 import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from '@apollo/client';
+import DOMPurify from 'dompurify'; // XSS sanitizer
 
-function App() {
-  return <div>Hello world na ja</div>;
+const client = new ApolloClient({
+  uri: '/api/graphql',
+  cache: new InMemoryCache()
+});
+
+const BLOG_POST_QUERY = gql`
+  query blogPostQuery {
+    blogPost {
+      title: displayText,
+      body: markdownBody {
+        html
+      }
+    }
+  }
+`;
+
+function BlogPost() {
+  const { loading, error, data } = useQuery(BLOG_POST_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const blogPostList = data.blogPost.map(({ title, body: { html } }) => {
+    const cleanHTML = DOMPurify.sanitize(html);
+    // https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
+
+    return (
+      <div>
+        <h2>{title}</h2>
+        <p dangerouslySetInnerHTML={{ __html: cleanHTML }} />
+      </div>
+    );
+  });
+
+  return blogPostList;
 }
 
-render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <BlogPost />
+    </div>
+  );
+}
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
